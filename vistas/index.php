9822ee12ad5ea92f5ae7747c758786e57d0ed756
carolinaflorentino3 <70494902+carolinaflorentino3@users.noreply.github.com>
2020-09-19 16:09:29 -0600

    Cambio titulo

--- vistas/index.php
+++ vistas/index.php
@@ -2,7 +2,7 @@
     <!DOCTYPE html>
     <html lang="en">
     <head>
-        <title>Sistema Luna Color</title>
+        <title>Luna Color</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, user-scalable=nom, initial-scale=1, maximum-scale=1, minimum-scale=1">
 
