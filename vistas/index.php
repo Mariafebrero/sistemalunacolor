f68f5b6b4a5fe6f06d3629679ecfc74deeb2a8db
carolinaflorentino3 <70494902+carolinaflorentino3@users.noreply.github.com>
2020-09-19 16:10:58 -0600

    cambio titulo login

--- vistas/index.php
+++ vistas/index.php
@@ -2,7 +2,7 @@
     <!DOCTYPE html>
     <html lang="en">
     <head>
-        <title>Luna Color</title>
+        <title>Sistema Luna Color</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, user-scalable=nom, initial-scale=1, maximum-scale=1, minimum-scale=1">
 
--- vistas/login.php
+++ vistas/login.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Sistema Luna Color</title>
+	<title>Luna Color</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
