62e414c9c972364d7fa877e8c262bc5bd50aad05
carolinaflorentino3 <70494902+carolinaflorentino3@users.noreply.github.com>
2020-10-26 01:51:43 -0600

    Facturación, BESSY y CAROL

--- vistas/factura.php
+++ vistas/factura.php
@@ -3,18 +3,16 @@
 ob_start();
 session_start();
 
-//nombre new variable para secion
-if (!isset($_SESSION["nombre_usuario"]))
+if (!isset($_SESSION["nombre"]))
 {
-  header("Location: login1.php");
+  header("Location: login.html");
 }
 else
 {
 require 'header.php';
 
-//if ($_SESSION['Factura']==1)
-//{
-
+if ($_SESSION['compras']==1)
+{
 ?>
 <!--Contenido-->
       <!-- Content Wrapper. Contains page content -->
@@ -25,43 +23,112 @@ require 'header.php';
               <div class="col-md-12">
                   <div class="box">
                     <div class="box-header with-border">
-                          <h1 class="box-title">Categoría <button class="btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Agregar</button></h1>
-                        <div class="box-tools pull-right">
-                        </div>
-                    </div>
+                        <center> <h1 ><span class="hiddenui"><i class="fas fa-file-alt"> Facturación</i></span></h1> </center>  
+                          <br>
+                            <button class="btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Agregar nueva factura</button> 
+                              <br>
+                                <br>
                     <!-- /.box-header -->
                     <!-- centro -->
                     <div class="panel-body table-responsive" id="listadoregistros">
                         <table id="tbllistado" class="table table-striped table-bordered table-condensed table-hover">
                           <thead>
                             <th>Opciones</th>
-                            <th>Nombre</th>
                             <th>Descripción</th>
+                            <th>Fecha</th>
                             <th>Estado</th>
                           </thead>
                           <tbody>                            
                           </tbody>
                           <tfoot>
                             <th>Opciones</th>
-                            <th>Nombre</th>
                             <th>Descripción</th>
+                            <th>Fecha</th>
                             <th>Estado</th>
                           </tfoot>
                         </table>
                     </div>
                     <div class="panel-body" style="height: 400px;" id="formularioregistros">
                         <form name="formulario" id="formulario" method="POST">
-                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <label>Nombre:</label>
-                            <input type="hidden" name="idcategoria" id="idcategoria">
-                            <input type="text" class="form-control" name="nombre" id="nombre" maxlength="50" placeholder="Nombre" required>
+                          <div class="form-group col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                            <label>Cliente:</label>
+                            <input type="hidden" name="idingreso" id="idingreso">
+                            <select id="idproveedor" name="idproveedor" class="form-control selectpicker" data-live-search="true" required>
+                               <option value="Boleta">Seleccione un cliente</option>
+                              
+                            </select>
                           </div>
-                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <label>Descripción:</label>
-                            <input type="text" class="form-control" name="descripcion" id="descripcion" maxlength="255" placeholder="Descripción">
+                          <div class="form-group col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                            <label>Fecha(*):</label>
+                            <input type="date" class="form-control" name="fecha_hora" id="fecha_hora" required="">
                           </div>
-                          <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Tipo de pago::</label>
+                            <select name="tipo_comprobante" id="tipo_comprobante" class="form-control selectpicker" required="">
+                                 <option value="Boleta">Seleccione una opción</option>
                         
+                            </select>
+                          </div>
+
+                            <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Pedido:</label>
+                            <select name="tipo_comprobante" id="tipo_comprobante" class="form-control selectpicker" required="">
+                               <option value="Boleta">Seleccione un pedido</option>
+                            </select>
+                          </div>
+
+                          <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                            <table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
+                              <thead style="background-color:#fcb072">
+                                    <th>Id Producto</th>
+                                    <th>Cantidad</th>
+                                    <th>Descripción</th>
+                                    <th>Precio Unitario</th>
+                                    <th>Subtotal</th>
+                                </thead>
+                                <tfoot>
+                                </tfoot>
+                                <tbody>
+                                  
+                                </tbody>
+                            </table>
+                          </div>
+
+                            <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Descuento:</label>
+                            <input type="text" class="form-control" name="impuesto" id="impuesto" required="">
+                          </div>
+
+                           <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Importe Gravado:</label>
+                            <input type="text" class="form-control" name="impuesto" id="impuesto" required="">
+                          </div>
+                           <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Importe Exento:</label>
+                            <input type="text" class="form-control" name="impuesto" id="impuesto" required="">
+                          </div>
+
+                           <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Importe Exonerado:</label>
+                            <input type="text" class="form-control" name="impuesto" id="impuesto" required="">
+                          </div>
+
+                           <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Impuesto:</label>
+                            <input type="text" class="form-control" name="impuesto" id="impuesto" required="">
+                          </div>
+
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Total:</label>
+                            <input type="text" class="form-control" name="impuesto" id="impuesto" required="">
+                          </div>
+
+                          <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                           
+                            <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-print"></i> Facturar</button>
+
+                            <button id="btnCancelar" class="btn btn-danger" onclick="cancelarform()" type="button"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
                           </div>
                         </form>
                     </div>
@@ -69,23 +136,62 @@ require 'header.php';
                   </div><!-- /.box -->
               </div><!-- /.col -->
           </div><!-- /.row -->
-
       </section><!-- /.content -->
 
     </div><!-- /.content-wrapper -->
   <!--Fin-Contenido-->
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title">Seleccione un Artículo</h4>
+        </div>
+        <div class="modal-body">
+          <table id="tblarticulos" class="table table-striped table-bordered table-condensed table-hover">
+            <thead>
+                <th>Opciones</th>
+                <th>Nombre</th>
+                <th>Categoría</th>
+                <th>Código</th>
+                <th>Stock</th>
+                <th>Imagen</th>
+            </thead>
+            <tbody>
+              
+            </tbody>
+            <tfoot>
+              <th>Opciones</th>
+                <th>Nombre</th>
+                <th>Categoría</th>
+                <th>Código</th>
+                <th>Stock</th>
+                <th>Imagen</th>
+            </tfoot>
+          </table>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+        </div>        
+      </div>
+    </div>
+  </div>  
+  <!-- Fin modal -->
 <?php
-//}
-//else
-//{
-  //require 'noacceso.php';
-//}
+}
+else
+{
+  require 'header.php';
+}
 
 require 'footer.php';
 ?>
-<!--<script type="text/javascript" src="scripts/categoria.js"></script>-->
-
+<script type="text/javascript" src="scripts/factura.js"></script>
 <?php 
 }
 ob_end_flush();
 ?>
+
+
